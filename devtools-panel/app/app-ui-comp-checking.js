0e3867c482c304c698a8da37f86aa129300766a3
Christian Vaas <christianvaas@auspex.eu>
2013-05-27 10:12:32 +0200

    Added dummy data for grid and real data for toolbar

--- devtools-panel/app/app-ui-comp-checking.js
+++ devtools-panel/app/app-ui-comp-checking.js
@@ -11,12 +11,13 @@ app.ui.comp.checking = cs.clazz({
     mixin: [ cs.marker.controller ],
     protos: {
         create: function () {
-            cs(this).create('model/toolbar', app.ui.widget.panel.toolbar.model, app.ui.widget.panel.toolbar.view)
+            cs(this).create('toolbarModel/view', app.ui.widget.toolbar.model, app.ui.widget.toolbar.view)
+            cs(this).create('gridModel/view', app.ui.widget.grid.model, app.ui.widget.grid.view)
 
             cs(this).model({
                 'event:clear'           : { value: false, valid: 'boolean', autoreset: true },
                 'event:check-journal'   : { value: false, valid: 'boolean', autoreset: true },
-                'data:continuous'      : { value: false, valid: 'boolean' }
+                'data:continuous'       : { value: false, valid: 'boolean' }
             })
         },
         prepare: function () {
@@ -29,21 +30,31 @@ app.ui.comp.checking = cs.clazz({
                 event: 'event:check-journal',
                 type: 'button'
             }, {
-                type: 'fill'
-            }, {
                 label: 'Continuous check',
                 data: 'data:continuous',
                 type: 'checkbox'
             }]
 
-            cs(this, 'model').value('data:items', toolbarItems)
+            cs(this, 'toolbarModel').value('data:items', toolbarItems)
+
+            var columns = ['Time', 'Source', 'ST', 'Origin', 'OT', 'Operation', 'Parameters']
+            var rows = [['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G'],['A','B','C','D','E','F','G']]
+
+            cs(this, 'gridModel').value('data:columns', columns)
+            cs(this, 'gridModel').value('data:rows', rows)
         },
         render: function () {
             var content = $.markup("checking-content")
             var self = this
 
             cs(self).socket({
-                ctx: content
+                scope: 'toolbarModel/view',
+                ctx: $('.toolbar', content)
+            })
+
+            cs(self).socket({
+                scope: 'gridModel/view',
+                ctx: $('.grid', content)
             })
 
             cs(self).plug(content)
